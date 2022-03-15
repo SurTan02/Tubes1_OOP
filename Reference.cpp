85ea288082b864eb437d4fdf8ad18a8aa08d7f5d
Vieri <73509485+VieriMansyl@users.noreply.github.com>
2022-03-15 21:24:33 +0700

    Export File (ver.1)
    command Export selesai
    improve tab line pada check.cpp dan main.cpp

---
+++ Reference.cpp
@@ -0,0 +1,6 @@
+//Reference.cpp : include implementasi dari class
+
+//Implementasi Class
+/*contoh :
+#include "Item.cpp"
+*/
--- main.cpp
+++ main.cpp
@@ -3,61 +3,81 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include "Reference.cpp"
 
 using namespace std;
 
 int main() {
-  string configPath = "./config";
-  string itemConfigPath = configPath + "/item.txt";
+	string configPath = "./config";
+	string itemConfigPath = configPath + "/item.txt";
 
-  // read item from config file
-  ifstream itemConfigFile(itemConfigPath);
-  for (string line; getline(itemConfigFile, line);) {
-    cout << line << endl;
-    // do something
-  }
+  	// read item from config file
+  	// ifstream itemConfigFile(itemConfigPath);
+  	// for (string line; getline(itemConfigFile, line);) {
+	// 	cout << line << endl;
+	// 	// do something
+  	// }
 
-  // read recipes
-  for (const auto &entry :
-       filesystem::directory_iterator(configPath + "/recipe")) {
-    cout << entry.path() << endl;
-    // read from file and do something
-  }
+	// // read recipes
+	// for (const auto &entry :
+	// 	filesystem::directory_iterator(configPath + "/recipe")) {
+	// 	cout << entry.path() << endl;
+	// 	// read from file and do something
+	// }
 
-  // sample interaction
-  string command;
-  while (cin >> command) {
-    if (command == "EXPORT") {
-      string outputPath;
-      cin >> outputPath;
-      ofstream outputFile(outputPath);
+	//INISIALISASI VARIABEL GLOBAL
+	Inventory inv;
 
-      // hardcode for first test case
-      outputFile << "21:10" << endl;
-      outputFile << "6:1" << endl;
-      for (int i = 2; i < 27; i++) {
-        outputFile << "0:0" << endl;
-      }
+	//COMMAND
+	string command;
+	cout << "Command : ";
+	while (cin >> command) {
+		if (command == "EXPORT") {
+			string outputPath;
+			cin >> outputPath;		//nama file
+			ofstream outputFile(outputPath);
 
-      cout << "Exported" << endl;
-    } else if (command == "CRAFT") {
-      cout << "TODO" << endl;
-    } else if (command == "GIVE") {
-      string itemName;
-      int itemQty;
-      cin >> itemName >> itemQty;
-      cout << "TODO" << endl;
-    } else if (command == "MOVE") {
-      string slotSrc;
-      int slotQty;
-      string slotDest;
-      // need to handle multiple destinations
-      cin >> slotSrc >> slotQty >> slotDest;
-      cout << "TODO" << endl;
-    } else {
-      // todo
-      cout << "Invalid command" << endl;
-    }
-  }
+			Item temp;
+			for(int i = 0 ; i < 27 ; i++){
+				temp = inv.getItem(i);
+				if(temp.getType() == "Tool"){		// Tool
+					outputFile << temp.getID() << ":" << temp.getDurability() << endl;
+				}else if(temp.getQuantity() > 0){	// Non Tool
+					outputFile << temp.getID() << ":" << temp.getQuantity() << endl;
+				}else{								// Tidak ada Item
+					outputFile << "0:0" << endl;
+				}
+			}
+			// // hardcode for first test case
+			// outputFile << "21:10" << endl;
+			// outputFile << "6:1" << endl;
+			// for (int i = 2; i < 27; i++) {
+			// 	outputFile << "0:0" << endl;
+			// }
+			cout << "Exported" << endl;
+			
+		} else if (command == "CRAFT") {
+			cout << "TODO" << endl;
+
+		} else if (command == "GIVE") {
+			string itemName;
+			int itemQty;
+			cin >> itemName >> itemQty;
+			cout << "TODO" << endl;
+
+		} else if (command == "MOVE") {
+			string slotSrc;
+			int slotQty;
+			string slotDest;
+			// need to handle multiple destinations
+			cin >> slotSrc >> slotQty >> slotDest;
+			cout << "TODO" << endl;
+
+		} else {
+			// todo
+			cout << "Invalid command" << endl;
+		}
+		cout << "Command : ";
+	}
   return 0;
 }
